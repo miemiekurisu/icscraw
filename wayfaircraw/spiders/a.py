aded512ae9f0ac2772db6402b9dd80605b4e7daf
miemiekurisu <miemiekurisu@aol.com>
2016-04-29 21:39:05 +0800

    123

--- wayfaircraw/spiders/a.py
+++
@@ -1,5 +0,0 @@
-mport os,commands,sys
- os.chdir('/user/akq/akq/caffe')
- sys.path.append('/user/akq/akq/caffe/extractFeature')
- commands.getoutput('export PATH=$PATH:/user/akq/akq/caffe/extractFeature')
-
